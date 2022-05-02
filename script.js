9197193d3cde2c58d38cfcdf090f909980302e03
FedericoAlippe <51132201+FedericoAlippe@users.noreply.github.com>
2022-05-01 23:38:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     if (principal <= 0){
         alert("Enter a positive number");
-        return 0;
+      
+    }else {
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
     }
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-   
 }
 function updateRate() 
 {
